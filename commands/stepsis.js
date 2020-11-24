f915e78e010812974e333e83404c60881588a949
Glitch (coordinated-educated-watercress) <none>
2020-11-24 23:39:16 +0000

    For Marcky.

---
+++ commands/stepsis.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+   return  message.channel.send("*what are you doing step bro!*")
+}
+
+module.exports.help = {
+    name: "stepsis"
+}
